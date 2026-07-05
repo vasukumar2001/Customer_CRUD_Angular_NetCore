47adc8e0ad2d696051b36fbd857a26486f900983
vasukumar2001 <vasufadadu123@gmail.com>
2026-07-05 11:55:56 +0530

    add Net Core API AdoNet

---
+++ CustomerApi_ADONet/CustomerApp/CustomerApp/Program.cs
@@ -0,0 +1,31 @@
+
+using CustomerApp.Api.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<AdoNetCustomerRepository>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAngular");
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
