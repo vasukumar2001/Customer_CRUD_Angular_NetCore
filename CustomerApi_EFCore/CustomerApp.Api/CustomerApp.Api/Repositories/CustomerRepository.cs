0df8bf3fac1efaaf57b10e9e5823b600983d9967
vasukumar2001 <vasufadadu123@gmail.com>
2026-07-05 11:59:15 +0530

    add Net Core API EF Core

---
+++ CustomerApi_EFCore/CustomerApp.Api/CustomerApp.Api/Repositories/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using CustomerApp.Api.Data;
+using CustomerApp.Api.Models;
+
+namespace CustomerApp.Api.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly CustomerDbContext _context;
+
+        public CustomerRepository(CustomerDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Customer> GetAll() => _context.Customers.ToList();
+
+        public Customer GetById(int id) => _context.Customers.Find(id);
+
+        public void Add(Customer customer) => _context.Customers.Add(customer);
+
+        public void Update(Customer customer) => _context.Customers.Update(customer);
+
+        public void Delete(Customer customer) => _context.Customers.Remove(customer);
+    }
+}
---
+++ CustomerApi_EFCore/CustomerApp.Api/CustomerApp.Api/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using CustomerApp.Api.Repositories;
+
+namespace CustomerApp.Api.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        ICustomerRepository Customers { get; }
+        int Complete();
+    }
+}
