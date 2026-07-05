56c53a5077fe48e593af5a58f4b6d2fafcd3d541
vasukumar2001 <vasufadadu123@gmail.com>
2026-07-05 11:57:50 +0530

    add Net Core API Dapper

---
+++ CustomerApi_Dapper/CustomerApp.Api/CustomerApp.Api/Validations/ValidEmailAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace CustomerApp.Api.Validations
+{
+    public class ValidEmailAttribute : ValidationAttribute
+    {
+        private static readonly Regex EmailRegex = new Regex(
+            @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+            RegexOptions.Compiled);
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            if (value == null) return ValidationResult.Success; 
+
+            string email = value.ToString();
+            if (!EmailRegex.IsMatch(email))
+                return new ValidationResult("Email format is invalid");
+
+            return ValidationResult.Success;
+        }
+    }
+}
